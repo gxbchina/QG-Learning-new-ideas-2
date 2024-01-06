28ff63b1f2e88cfa9aef50341a3083f01aeb29d0
Ren3030 <119498971+Ren3030@users.noreply.github.com>
2024-01-07 05:50:27 +0800

    Update 0.js

--- 0.js
+++ 0.js
@@ -1568,14 +1568,13 @@ function do_exec(type) {
   ran_sleep();
   // 等待加载
   text("提示").waitFor();
-
   // 判断题型
   /******************单选题*******************/
   if (textStartsWith("单选题").exists()) {
     // 获取题目
     let que_txt = className("android.view.View").depth(10).findOnce(1).text();
     // 上面被专项答题影响了22、23层的元素数，只能通过其他层定位
-    //let que_txt = className("android.view.View").depth(11).findOnce(1).parent().parent().child(1).text();
+    //let que_txt = className("android.view.View").depth(9).findOnce(1).parent().parent().child(1).text();
     log(que_txt);
     var ans = get_ans_by_re(que_txt);
     if (ans && depth(26).text(ans).exists()) {
@@ -1709,7 +1708,7 @@ function do_exec(type) {
     // 获取题目
     let que_txt = className("android.view.View").depth(10).findOnce(1).text();
     // 上面被专项答题影响了22、23层的元素数，只能通过其他层定位
-    //let que_txt = className("android.view.View").depth(11).findOnce(1).parent().parent().child(1).text();
+    //let que_txt = className("android.view.View").depth(10).findOnce(1).parent().parent().child(1).text();
     log(que_txt);
     // 这里匹配出全部挖空
     let reg1 = /\s{3,}/g;

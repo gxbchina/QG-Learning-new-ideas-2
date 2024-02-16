0a944f0a59106c924dfeeb1eac8e1f818668a519
Ren3030 <119498971+Ren3030@users.noreply.github.com>
2024-02-16 19:06:30 +0800

    Update 0.js

--- 0.js
+++ 0.js
@@ -160,7 +160,7 @@ device.keepScreenOn(3600 * 1000);
 // 下载题库
 fInfo("检测题库更新");
 fInfo("如果不动就是正在下载，多等会");
-const update_info = get_tiku_by_http("https://gitcode.net/m0_64980826/songge_tiku/-/blob/master/info.json");
+const update_info = get_tiku_by_http("https://gitcode.net/m0_64980826/songge_tiku/-/raw/master/info.json");
 //const update_info = get_tiku_by_http("https://gitcode.net/m0_64980826/songge_tiku/-/raw/master/info.json");
 fInfo("正在加载对战题库......请稍等\n题库版本:" + update_info["tiku_link"].split('_')[1]);
 var tiku = [];

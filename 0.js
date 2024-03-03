b56a2f029a970b560724fb4cd98d9ed3834baad4
Ren3030 <119498971+Ren3030@users.noreply.github.com>
2024-03-04 00:11:05 +0800

    Update 0.js

--- 0.js
+++ 0.js
@@ -370,8 +370,8 @@ function do_wenzhang() {
   //晚12点后
   //let listview = className("androidx.recyclerview.widget.RecyclerView").findOne();
   //白天未知
-  //let listview = className("android.widget.ListView").findOne();
-  let listview = rowCount("14").depth("17").findOne();
+  let listview = className("android.widget.ListView").findOne();
+  //let listview = rowCount("14").depth("17").findOne();
   fInfo("1111正常");
   for (i = 0; i < 2; i++) {
     listview.scrollForward();

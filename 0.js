0660af71c212886daba8fe15abe30dbdfea9d2e6
Ren3030 <119498971+Ren3030@users.noreply.github.com>
2024-03-04 03:23:35 +0800

    Update 0.js

--- 0.js
+++ 0.js
@@ -367,9 +367,9 @@ function do_wenzhang() {
   fInfo("识别出顶部：", swipe_y);
   fRefocus();
   //晚12点后
-  let listview = className("androidx.recyclerview.widget.RecyclerView").findOne();
+  //let listview = className("androidx.recyclerview.widget.RecyclerView").findOne();
   //白天未知
-  //let listview = className("android.widget.ListView").depth(17).findOne();
+  let listview = className("android.widget.ListView").depth(17).findOne();
   for (i = 0; i < 2; i++) {
     listview.scrollForward();
     sleep(500);
